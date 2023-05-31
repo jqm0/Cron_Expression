73caab184e9dca452516fe8fbd3967df71e26259
jqm0 <121156533+jqm0@users.noreply.github.com>
2023-05-30 23:30:30 -0700

    initialize the project

---
+++ cron_Expression/AutoCommitScript.bat
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd C:\Users\Lenovo\Documents\GitHub\Todo_List_Manager 
+
+# Add your files to the repository
+git add .
+
+# Commit the changes
+git commit -m "Remove Bugs from the code and test for 6th scenario (Todo Deletion) " --allow-empty
+
+# Push the changes to GitHub
+git push
---
+++ cron_Expression/src/main/java/com/example/cron_Expression/CronExpressionApplication.java
@@ -0,0 +1,13 @@
+package com.example.cron_Expression;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CronExpressionApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(CronExpressionApplication.class, args);
+	}
+
+}
