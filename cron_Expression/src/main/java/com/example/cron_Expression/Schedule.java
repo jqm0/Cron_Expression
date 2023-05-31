d0e8701e9d28aac830cb805d30aabd0f3a5f2c3a
jqm0 <121156533+jqm0@users.noreply.github.com>
2023-05-31 04:05:36 -0700

    Task 32

--- cron_Expression/src/main/java/com/example/cron_Expression/Schedule.java
+++ cron_Expression/src/main/java/com/example/cron_Expression/Schedule.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Schedule {
-    @Scheduled(cron =  "0 30 23 * * 5")
+    @Scheduled(cron =  "0 15 3 * * *")
     public void display(){
         System.out.println("Drink water");
     }
