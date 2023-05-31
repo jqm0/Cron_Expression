92171e73bd8757cf4624031ad84cf916cd13f743
jqm0 <121156533+jqm0@users.noreply.github.com>
2023-05-31 01:06:03 -0700

    Task 18

--- cron_Expression/src/main/java/com/example/cron_Expression/Schedule.java
+++ cron_Expression/src/main/java/com/example/cron_Expression/Schedule.java
@@ -5,14 +5,10 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Schedule {
-    // Weekdays
-    @Scheduled(cron =  "*/5 * * * 1-5")
+    //5 for friday
+    @Scheduled(cron =  "0 30 23 13 * 5")
     public void display(){
         System.out.println("Drink water");
     }
-    // Weekends
-    @Scheduled(cron =  "*/15 * * * 6-7")
-    public void displayforWeekends(){
-        System.out.println("Drink water");
-    }
+
 }
