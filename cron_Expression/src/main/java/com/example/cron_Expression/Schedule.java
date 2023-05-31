33bfb10f08d0fca8a142158d7778d07205c60508
jqm0 <121156533+jqm0@users.noreply.github.com>
2023-05-31 04:02:42 -0700

    Update Schedule.java

--- cron_Expression/src/main/java/com/example/cron_Expression/Schedule.java
+++ cron_Expression/src/main/java/com/example/cron_Expression/Schedule.java
@@ -5,9 +5,10 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Schedule {
-    @Scheduled(cron =  "0 0 12 1-31 7 * 1")
+    @Scheduled(cron =  "*/5 * 1 * *")
     public void display(){
         System.out.println("Drink water");
     }
+
 
 }
